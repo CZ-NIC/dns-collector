7e769f8c4092b9538a497f0846a9165759b1fee2
Martin Mares <mj@ucw.cz>
2001-01-21 17:59:33 +0000

    Created a header which will contain description of all data structures used in the indices. For now, there are word categories.

---
+++ lib/index.h
@@ -0,0 +1,22 @@
+/*
+ *	Sherlock Gatherer: Data structures used in indices
+ *
+ *	(c) 2001 Martin Mares <mj@ucw.cz>
+ */
+
+/* Words */
+
+#define MAX_WORD_LEN		64
+
+/* Word types */
+
+enum word_type {
+  WT_TEXT,				/* Ordinary text */
+  WT_EMPH,				/* Emphasized text */
+  WT_TITLE,				/* Document title */
+  WT_SMALL_HEADING,			/* Heading */
+  WT_BIG_HEADING,			/* Larger heading */
+  WT_KEYWORD,				/* Explicitly marked keyword */
+  WT_META,				/* Various meta-information */
+  WT_ALT				/* Alternate texts for graphical elements */
+};
