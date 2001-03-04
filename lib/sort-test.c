3fb4a33e0fdaa86984dd3918eb6a1cd345936e07
Robert Spalek <robert@ucw.cz>
2001-03-04 15:21:50 +0000

    cf_default_{init,done} interface used instead cf_read

--- lib/sort-test.c
+++ lib/sort-test.c
@@ -77,10 +77,11 @@ int
 main(int argc, char **argv)
 {
   log_init(NULL);
-  cf_read(DEFAULT_CONFIG);
+  cf_default_init(DEFAULT_CONFIG);
   if (cf_getopt(argc, argv, CF_SHORT_OPTS, CF_NO_LONG_OPTS, NULL) >= 0 ||
       optind != argc - 2)
     die("Usage: sort-test <input> <output>");
+  cf_default_done();
 
   s_sort(argv[optind], argv[optind+1]);
   return 0;
