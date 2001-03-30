57fe9acaa145c5c7a6adeeed05d700888ef594ca
Robert Spalek <robert@ucw.cz>
2001-03-30 13:20:14 +0000

    test audited

--- lib/regex-test.c
+++ lib/regex-test.c
@@ -8,23 +8,24 @@
 
 #include <stdio.h>
 
-#define	PREPARE(patt, icase)	r = rx_compile(patt, icase); printf("\npattern: %s\n", patt)
-#define	TEST(txt, should)	printf(#txt ": %d (should %d)\n", rx_match(r, #txt), should)
+#define	PREPARE(patt, icase)	r = rx_compile(patt, icase); printf("\npattern: %s, icase=%d\n", patt, icase)
+#define	TEST(txt, should)	printf(txt ": %d (should %d)\n", rx_match(r, txt), should)
+
 int
 main(void)
 {
 	regex *r;
 
 	PREPARE("a.*b.*c", 0);
-	TEST(abc, 1);
-	TEST(ajkhkbbbbbc, 1);
-	TEST(Aabc, 0);
+	TEST("abc", 1);
+	TEST("ajkhkbbbbbc", 1);
+	TEST("Aabc", 0);
 	rx_free(r);
 
 	PREPARE("a.*b.*c", 1);
-	TEST(aBc, 1);
-	TEST(ajkhkbBBBBC, 1);
-	TEST(Aabc, 1);
+	TEST("aBc", 1);
+	TEST("ajkhkbBBBBC", 1);
+	TEST("Aabc", 1);
 	rx_free(r);
 
 	PREPARE("(ahoj|nebo)", 1);
@@ -32,6 +33,11 @@ main(void)
 	TEST("nEBo", 1);
 	TEST("ahoja", 0);
 	TEST("(ahoj|nebo)", 0);
+	rx_free(r);
+
+	PREPARE("\\(ahoj\\)", 0);
+	TEST("(ahoj)", 1);
+	TEST("ahoj", 0);
 	rx_free(r);
 
 	return 0;
