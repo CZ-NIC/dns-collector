0eea3757c0ee4462a641562ba03c893548971d8f
Martin Mares <mj@ucw.cz>
2002-04-25 17:37:11 +0000

    Implemented base-224 encoder and decoder.

---
+++ lib/base224.h
@@ -0,0 +1,22 @@
+/*
+ *	Base 224 Encoding & Decoding
+ *
+ *	(c) 2002 Martin Mares <mj@ucw.cz>
+ */
+
+uns base224_encode(byte *dest, byte *src, uns len);
+uns base224_decode(byte *dest, byte *src, uns len);
+
+/*
+ * Warning: when encoding, at least 4 bytes of extra space are needed.
+ * Better use this macro to calculate buffer size.
+ */
+#define BASE224_ENC_LENGTH(x) (((x)*8+38)/39*5)
+
+/*
+ * When called for BASE224_IN_CHUNK-byte chunks, the result will be
+ * always BASE224_OUT_CHUNK bytes long. If a longer block is split
+ * to such chunks, the result will be identical.
+ */
+#define BASE224_IN_CHUNK 39
+#define BASE224_OUT_CHUNK 40
