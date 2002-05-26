4fb49f70bd657ec07d083ab03db9c61b984a55f3
Martin Mares <mj@ucw.cz>
2002-05-26 18:23:26 +0000

    Index reftexts, but don't search in them by default.

--- lib/custom.h
+++ lib/custom.h
@@ -35,7 +35,7 @@ enum word_type {
 
 /* Keywords for word type names */
 #define WORD_TYPE_NAMES	       			\
-	T(WORD, ~0)				\
+	T(WORD, ~((1 << WT_FILE) | (1 << WT_LINK)))	\
 	T(TEXT, 1 << WT_TEXT)			\
 	T(EMPH, 1 << WT_EMPH)			\
 	T(SMALL, 1 << WT_SMALL)			\
