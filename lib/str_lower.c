4a18657738121723a93fa7bb941ab168374954ad
Martin Mares <mj@ucw.cz>
2004-02-25 20:30:50 +0000

    Forgot to add this one.

---
+++ lib/str_lower.c
@@ -0,0 +1,16 @@
+/*
+ *	Sherlock Library -- Lowercase Map
+ *
+ *	(c) 1997--2004 Martin Mares <mj@ucw.cz>
+ *
+ *	This software may be freely distributed and used according to the terms
+ *	of the GNU Lesser General Public License.
+ */
+
+#include "lib/chartype.h"
+
+const unsigned char _c_lower[256] = {
+#define CHAR(code,upper,lower,cat) lower,
+#include "lib/charmap.h"
+#undef CHAR
+};
