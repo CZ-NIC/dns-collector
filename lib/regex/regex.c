c69a0f4956638e3e45c7603aa14513cca76fa5e6
Martin Mares <mj@ucw.cz>
2004-03-20 23:22:52 +0000

    After another couple of hours spent digging in regular expression libraries I decided to use a copy of the glibc 2.3.2 regex routines.

---
+++ lib/regex/regex.c
@@ -0,0 +1,10 @@
+/*
+ *  Regular Expression Functions from glibc 2.3.2
+ */
+
+#include <sys/types.h>
+#include "regex-sh.h"
+#include "regex_internal.h"
+#include "regex_internal.c"
+#include "regcomp.c"
+#include "regexec.c"
