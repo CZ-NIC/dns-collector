0f2719c181f3a8a9ba88ef76901f2115dee88c48
Martin Mares <mj@ucw.cz>
2006-11-25 23:35:55 +0100

    Implemented an asynchronous I/O library module.
    It's built at the top of the work queue module.

--- lib/asio.h
+++ lib/asio.h
@@ -7,51 +7,51 @@
  *	of the GNU Lesser General Public License.
  */
 
-#ifndef _UCW_ASYNCIO_H
-#define _UCW_ASYNCIO_H
+#ifndef _UCW_ASIO_H
+#define _UCW_ASIO_H
 
-#include "lib/semaphore.h"
+#include "lib/workqueue.h"
 #include "lib/clists.h"
 
 /* FIXME: Comment, especially on request ordering */
 
 struct asio_queue {
-  uns max_requests;			// Maximum number of requests allowed on this queue [user-settable]
   uns buffer_size;			// How large buffers do we use [user-settable]
+  uns max_writebacks;			// Maximum number of writeback requests active [user-settable]
   uns allocated_requests;
+  uns allocated_writebacks;
   uns running_requests;
   clist idle_list;			// Recycled requests waiting for get
-  clist done_list;			// Requests returned from the worker threads
-  sem_t *done_sem;			// ... and how many of them
-  clist wait_list;			// Requests available for wait()
+  clist done_list;			// Finished requests
+  struct work_queue queue;
 };
 
 enum asio_op {
   ASIO_FREE,
   ASIO_READ,
   ASIO_WRITE,
-  ASIO_BACKGROUND_WRITE,		// Write with no success notification
+  ASIO_WRITE_BACK,			// Write with no success notification
 };
 
 struct asio_request {
-  cnode n;
+  struct work work;			// asio_requests are internally just work nodes
   struct asio_queue *queue;
   byte *buffer;
   int fd;
   enum asio_op op;
   uns len;
   int status;
+  int returned_errno;
 };
 
-void asio_init(void);
-void asio_cleanup(void);
-
 void asio_init_queue(struct asio_queue *q);			// Initialize a new queue
-struct asio_request *asio_get(struct asio_queue *q);		// Get an empty request
+void asio_cleanup_queue(struct asio_queue *q);
+struct asio_request *asio_get(struct asio_queue *q);		// Get an empty request (not for writeback)
+struct asio_request *asio_get_writeback(struct asio_queue *q);	// Get an empty writeback request
+void asio_turn_to_writeback(struct asio_request *r);		// Convert a request allocated as non-writeback to writeback
 void asio_submit(struct asio_request *r);			// Submit the request
 struct asio_request *asio_wait(struct asio_queue *q);		// Wait for the first finished request, NULL if no more
 void asio_put(struct asio_request *r);				// Return a finished request for recycling
-struct asio_request *asio_get_bg(struct asio_queue *q);		// Get and if there are no free requests, wait for background writes to finish
-void asio_sync(struct asio_queue *q);				// Wait for all requests to finish
+void asio_sync(struct asio_queue *q);				// Wait until all requests are finished
 
-#endif	/* !_UCW_ASYNCIO_H */
+#endif	/* !_UCW_ASIO_H */
