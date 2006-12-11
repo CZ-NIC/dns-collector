2e228d21a4643c28db7ba87dbc5f4fc523ae4fe1
Martin Mares <mj@ucw.cz>
2006-12-11 23:43:37 +0100

    Added threading infrastructure to libucw and CONFIG_UCW_THREADS.
    Finally decided that we need threading support in too many libucw modules,
    so the only sane thing is to link all programs with -lpthread. On the
    other hand, I would like to keep the possibility of building non-threaded libucw,
    so I've introduced a new config switch, which is always set for Sherlock.

---
+++ lib/threads.h
@@ -0,0 +1,34 @@
+/*
+ *	The UCW Library -- Threading Helpers
+ *
+ *	(c) 2006 Martin Mares <mj@ucw.cz>
+ *
+ *	This software may be freely distributed and used according to the terms
+ *	of the GNU Lesser General Public License.
+ */
+
+#ifndef _UCW_THREAD_H
+#define _UCW_THREAD_H
+
+/* This structure holds per-thread data */
+
+struct ucwlib_context {
+  int temp_counter;			// Counter for fb-temp.c
+  struct asio_queue *io_queue;		// Async I/O queue for fb-direct.c
+  sh_sighandler_t *signal_handlers;	// Signal handlers for sighandler.c
+};
+
+struct ucwlib_context *ucwlib_thread_context(void);
+
+/* Global lock used for initialization, cleanup and other not so frequently accessed global state */
+
+void ucwlib_lock(void);
+void ucwlib_unlock(void);
+
+#ifdef CONFIG_UCW_THREADS
+
+extern uns default_thread_stack_size;
+
+#endif
+
+#endif
