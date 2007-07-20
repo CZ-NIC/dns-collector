2e49e3c1bb0d46a29108da2e6d489507893304f4
Martin Mares <mj@ucw.cz>
2007-07-20 16:35:45 +0200

    CGI: Added parsing of multi-part POSTed forms (file uploads etc.)
    In addition to that, we accept parameters listed in URL not only for
    GET requests, but also for POSTs.

--- lib/perl/CGI.pm
+++ lib/perl/CGI.pm
@@ -110,8 +110,15 @@ sub parse_arg_string($) {
 	}
 }
 
+sub parse_multipart_form_data();
+
 sub parse_args($) {
 	$arg_table = shift @_;
+	if (!defined $ENV{"GATEWAY_INTERFACE"}) {
+		print STDERR "Must be called as a CGI script.\n";
+		$exit_code = 1;
+		exit;
+	}
 	foreach my $a (values %$arg_table) {
 		my $r = ref($a->{'var'});
 		defined($a->{'default'}) or $a->{'default'}="";
@@ -121,22 +128,205 @@ sub parse_args($) {
 			@{$a->{'var'}} = ();
 		}
 	}
-	defined $ENV{"GATEWAY_INTERFACE"} or die "Not called as a CGI script";
 	my $method = $ENV{"REQUEST_METHOD"};
+	my $qs = $ENV{"QUERY_STRING"};
+	parse_arg_string($qs) if defined($qs);
 	if ($method eq "GET") {
-		parse_arg_string($ENV{"QUERY_STRING"});
 	} elsif ($method eq "POST") {
 		if ($ENV{"CONTENT_TYPE"} =~ /^application\/x-www-form-urlencoded\b/i) {
 			while (<STDIN>) {
 				chomp;
 				parse_arg_string($_);
 			}
+		} elsif ($ENV{"CONTENT_TYPE"} =~ /^multipart\/form-data\b/i) {
+			parse_multipart_form_data();
 		} else {
-			return "Unknown content type for POST data";
+			die "Unknown content type for POST data";
 		}
 	} else {
-		return "Unknown request method";
+		die "Unknown request method";
 	}
+}
+
+### Parsing Multipart Form Data ###
+
+my $boundary;
+my $boundary_len;
+my $mp_buffer;
+my $mp_buffer_i;
+my $mp_buffer_boundary;
+my $mp_eof;
+
+sub refill_mp_data($) {
+	my ($more) = @_;
+	if ($mp_buffer_boundary >= $mp_buffer_i) {
+		return $mp_buffer_boundary - $mp_buffer_i;
+	} elsif ($mp_buffer_i + $more <= length($mp_buffer) - $boundary_len) {
+		return $more;
+	} else {
+		if ($mp_buffer_i) {
+			$mp_buffer = substr($mp_buffer, $mp_buffer_i);
+			$mp_buffer_i = 0;
+		}
+		while ($mp_buffer_i + $more > length($mp_buffer) - $boundary_len) {
+			last if $mp_eof;
+			my $data;
+			my $n = read(STDIN, $data, 2048);
+			if ($n > 0) {
+				$mp_buffer .= $data;
+			} else {
+				$mp_eof = 1;
+			}
+		}
+		$mp_buffer_boundary = index($mp_buffer, $boundary, $mp_buffer_i);
+		if ($mp_buffer_boundary >= 0) {
+			return $mp_buffer_boundary;
+		} elsif ($mp_eof) {
+			return length($mp_buffer);
+		} else {
+			return length($mp_buffer) - $boundary_len;
+		}
+	}
+}
+
+sub get_mp_line($) {
+	my ($allow_empty) = @_;
+	my $n = refill_mp_data(1024);
+	my $i = index($mp_buffer, "\r\n", $mp_buffer_i);
+	if ($i >= $mp_buffer_i && $i < $mp_buffer_i + $n - 1) {
+		my $s = substr($mp_buffer, $mp_buffer_i, $i - $mp_buffer_i);
+		$mp_buffer_i = $i + 2;
+		return $s;
+	} elsif ($allow_empty) {
+		if ($n) {							# An incomplete line
+			my $s = substr($mp_buffer, $mp_buffer_i, $n);
+			$mp_buffer_i += $n;
+			return $s;
+		} else {							# No more lines
+			return undef;
+		}
+	} else {
+		die "Premature end of multipart POST data";
+	}
+}
+
+sub skip_mp_boundary() {
+	if ($mp_buffer_boundary != $mp_buffer_i) {
+		die "Premature end of multipart POST data";
+	}
+	$mp_buffer_boundary = -1;
+	$mp_buffer_i += 2;
+	my $b = get_mp_line(0);
+	print STDERR "SEP $b\n" if $debug;
+	$mp_buffer_boundary = index($mp_buffer, $boundary, $mp_buffer_i);
+	if ("\r\n$b" =~ /^$boundary--/) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+sub parse_mp_header() {
+	my $h = {};
+	my $last;
+	while ((my $l = get_mp_line(0)) ne "") {
+		print STDERR "HH $l\n" if $debug;
+		if (my ($name, $value) = ($l =~ /([A-Za-z0-9-]+)\s*:\s*(.*)/)) {
+			$name =~ tr/A-Z/a-z/;
+			$h->{$name} = $value;
+			$last = $name;
+		} elsif ($l =~ /^\s+/ && $last) {
+			$h->{$last} .= $l;
+		} else {
+			$last = undef;
+		}
+	}
+	foreach my $n (keys %$h) {
+		$h->{$n} = rfc822_prepare($h->{$n});
+		print STDERR "H $n: $h->{$n}\n" if $debug;
+	}
+	return (keys %$h) ? $h : undef;
+}
+
+sub parse_multipart_form_data() {
+	# First of all, find the boundary string
+	my $ct = rfc822_prepare($ENV{"CONTENT_TYPE"});
+	if (!(($boundary) = ($ct =~ /^.*;boundary=([^; ]+)/))) {
+		die "Multipart content with no boundary string received";
+	}
+	$boundary = rfc822_deescape($boundary);
+	print STDERR "BOUNDARY IS $boundary\n" if $debug;
+
+	# BUG: IE 3.01 on Macintosh forgets to add the "--" at the start of the boundary string
+	# as the MIME specs preach. Workaround borrowed from CGI.pm in Perl distribution.
+	my $agent = http_get("User-agent") || "";
+	$boundary = "--$boundary" unless $agent =~ /MSIE\s+3\.0[12];\s*Mac/;
+	$boundary = "\r\n$boundary";
+	$boundary_len = length($boundary) + 2;
+
+	# Check upload size in advance
+	if (my $size = http_get("Content-Length")) {
+		my $max_allowed = 0;
+		foreach my $a (values %$arg_table) {
+			$max_allowed += $a->{"maxsize"} || 65536;
+		}
+		if ($size > $max_allowed) {
+			die "Maximum form data length exceeded";
+		}
+	}
+
+	# Initialize our buffering mechanism and part splitter
+	$mp_buffer = "\r\n";
+	$mp_buffer_i = 0;
+	$mp_buffer_boundary = -1;
+	$mp_eof = 0;
+
+	# Skip garbage before the 1st part
+	while (my $i = refill_mp_data(256)) { $mp_buffer_i += $i; }
+	skip_mp_boundary() || return;
+
+	# Process individual parts
+	do { PART: {
+		print STDERR "NEXT PART\n" if $debug;
+		my $h = parse_mp_header();
+		my ($field, $cdisp, $a);
+		if ($h &&
+		    ($cdisp = $h->{"content-disposition"}) &&
+		    $cdisp =~ /^form-data/ &&
+		    (($field) = ($cdisp =~ /;name=([^;]+)/)) &&
+		    ($a = $arg_table->{"$field"})) {
+			print STDERR "FIELD $field\n" if $debug;
+			if (defined $h->{"content-transfer-encoding"}) { die "Unexpected Content-Transfer-Encoding"; }
+			if (defined $a->{"var"}) {
+				while (defined (my $l = get_mp_line(1))) {
+					print STDERR "VALUE $l\n" if $debug;
+					parse_arg_string("$field=$l");
+				}
+				next PART;
+			} elsif (defined $a->{"file"}) {
+				require File::Temp;
+				require IO::Handle;
+				my $max_size = $a->{"maxsize"} || 1048576;
+				my @tmpargs = (undef, UNLINK => 1);
+				push @tmpargs, DIR => $a->{"tmpdir"} if defined $a->{"tmpdir"};
+				my ($fh, $fn) = File::Temp::tempfile(@tmpargs);
+				print STDERR "FILE UPLOAD to $fn\n" if $debug;
+				${$a->{"file"}} = $fn;
+				${$a->{"fh"}} = $fh if defined $a->{"fh"};
+				my $total_size = 0;
+				while (my $i = refill_mp_data(4096)) {
+					print $fh substr($mp_buffer, $mp_buffer_i, $i);
+					$mp_buffer_i += $i;
+					$total_size += $i;
+					if ($total_size > $max_size) { die "Uploaded file too long"; }
+				}
+				$fh->flush();	# Don't close the handle, the file would disappear otherwise
+				next PART;
+			}
+		}
+		print STDERR "SKIPPING\n" if $debug;
+		while (my $i = refill_mp_data(256)) { $mp_buffer_i += $i; }
+	} } while (skip_mp_boundary());
 }
 
 ### Generating Self-ref URL's ###
