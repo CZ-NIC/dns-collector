7ebe76cbc0bf64a033e9d978dd22cea3257e128b
Martin Mares <mj@ucw.cz>
2007-09-10 13:38:43 +0200

    Slightly improved user interface of {asio,file}-test and renamed them to radix-*.

--- debug/sorter/radix-file-test.c
+++ debug/sorter/radix-file-test.c
@@ -1,19 +1,18 @@
 /*
- *  An experiment with parallel reading and writing of files using ASIO.
+ *  An experiment with parallel reading and writing of files.
  */
 
 #include "lib/lib.h"
+#include "lib/conf.h"
 #include "lib/lfs.h"
-#include "lib/asio.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #define COPY
-#define DIRECT O_DIRECT
+#define DIRECT 0		// or O_DIRECT
 
 static timestamp_t timer;
 
@@ -28,45 +27,41 @@ static timestamp_t timer;
   msg(L_INFO, "Spent %.3f sec (%.2f MB/sec)", (double)cnt_ms/1000, (double)cnt / 1048576 * 1000 / cnt_ms); \
 } while(0)
 
-static struct asio_queue io_queue;
-
 int main(int argc, char **argv)
 {
-  ASSERT(argc == 4);
-  uns files = atol(argv[1]);
-  uns bufsize = atol(argv[2]) * 1024;				// Kbytes
-  u64 total_size = (u64)atol(argv[3]) * 1024*1024*1024;		// Gbytes
+  uns files, bufsize;
+  u64 total_size;
+  if (argc != 4 ||
+      cf_parse_int(argv[1], (int*) &files) ||
+      cf_parse_int(argv[2], (int*) &bufsize) ||
+      cf_parse_u64(argv[3], &total_size))
+    {
+      fprintf(stderr, "Usage: file-test <nr-files> <bufsize> <totalsize>\n");
+      return 1;
+    }
   u64 cnt, cnt_rep;
   uns cnt_ms;
   int fd[files];
-  byte name[files][16];
-  struct asio_request *req[files];
+  byte *buf[files], name[files][16];
+  uns xbufsize = bufsize;					// Used for single-file I/O
+  byte *xbuf = big_alloc(xbufsize);
 
   init_timer(&timer);
-
-  io_queue.buffer_size = bufsize;
-  io_queue.max_writebacks = 2;
-  asio_init_queue(&io_queue);
 
 #ifdef COPY
   msg(L_INFO, "Creating input file");
   int in_fd = sh_open("tmp/ft-in", O_RDWR | O_CREAT | O_TRUNC | DIRECT, 0666);
   ASSERT(in_fd >= 0);
-  ASSERT(!(total_size % bufsize));
+  ASSERT(!(total_size % xbufsize));
   P_INIT;
-  for (uns i=0; i<total_size/bufsize; i++)
+  for (uns i=0; i<total_size/xbufsize; i++)
     {
-      struct asio_request *r = asio_get(&io_queue);
-      r->op = ASIO_WRITE_BACK;
-      r->fd = in_fd;
-      r->len = bufsize;
-      byte *xbuf = r->buffer;
-      for (uns j=0; j<bufsize; j++)
+      for (uns j=0; j<xbufsize; j++)
 	xbuf[j] = i+j;
-      asio_submit(r);
-      P_UPDATE(bufsize);
+      uns c = write(in_fd, xbuf, xbufsize);
+      ASSERT(c == xbufsize);
+      P_UPDATE(c);
     }
-  asio_sync(&io_queue);
   lseek(in_fd, 0, SEEK_SET);
   sync();
   P_FINAL;
@@ -79,44 +74,29 @@ int main(int argc, char **argv)
       fd[i] = sh_open(name[i], O_RDWR | O_CREAT | O_TRUNC | DIRECT, 0666);
       if (fd[i] < 0)
 	die("Cannot create %s: %m", name[i]);
+      buf[i] = big_alloc(bufsize);
     }
   sync();
   get_timer(&timer);
 
   msg(L_INFO, "Writing %d MB to %d files in parallel with %d byte buffers", (int)(total_size >> 20), files, bufsize);
   P_INIT;
-  for (uns i=0; i<files; i++)
-    req[i] = asio_get(&io_queue);
-  for (uns round=0; round<total_size/bufsize/files; round++)
+  for (uns r=0; r<total_size/bufsize/files; r++)
     {
       for (uns i=0; i<files; i++)
 	{
-	  struct asio_request *r = req[i];
 #ifdef COPY
-	  struct asio_request *rr, *rd = asio_get(&io_queue);
-	  rd->op = ASIO_READ;
-	  rd->fd = in_fd;
-	  rd->len = bufsize;
-	  asio_submit(rd);
-	  rr = asio_wait(&io_queue);
-	  ASSERT(rr == rd && rd->status == (int)rd->len);
-	  memcpy(r->buffer, rd->buffer, bufsize);
-	  asio_put(rr);
+	  uns ci = read(in_fd, buf[i], bufsize);
+	  ASSERT(ci == bufsize);
 #else
 	  for (uns j=0; j<bufsize; j++)
-	    r->buffer[j] = round+i+j;
+	    buf[i][j] = r+i+j;
 #endif
-	  r->op = ASIO_WRITE_BACK;
-	  r->fd = fd[i];
-	  r->len = bufsize;
-	  asio_submit(r);
-	  P_UPDATE(bufsize);
-	  req[i] = asio_get(&io_queue);
+	  uns c = write(fd[i], buf[i], bufsize);
+	  ASSERT(c == bufsize);
+	  P_UPDATE(c);
 	}
     }
-  for (uns i=0; i<files; i++)
-    asio_put(req[i]);
-  asio_sync(&io_queue);
 #ifdef COPY
   close(in_fd);
 #endif
@@ -129,17 +109,11 @@ int main(int argc, char **argv)
   for (uns i=0; i<files; i++)
     {
       lseek(fd[i], 0, SEEK_SET);
-      for (uns round=0; round<total_size/bufsize/files; round++)
+      for (uns r=0; r<total_size/xbufsize/files; r++)
 	{
-	  struct asio_request *rr, *r = asio_get(&io_queue);
-	  r->op = ASIO_READ;
-	  r->fd = fd[i];
-	  r->len = bufsize;
-	  asio_submit(r);
-	  rr = asio_wait(&io_queue);
-	  ASSERT(rr == r && r->status == (int)bufsize);
-	  asio_put(r);
-	  P_UPDATE(bufsize);
+	  uns c = read(fd[i], xbuf, xbufsize);
+	  ASSERT(c == xbufsize);
+	  P_UPDATE(c);
 	}
       close(fd[i]);
     }
@@ -150,8 +124,6 @@ int main(int argc, char **argv)
 #ifdef COPY
   unlink("tmp/ft-in");
 #endif
-
-  asio_cleanup_queue(&io_queue);
   msg(L_INFO, "Done");
   return 0;
 }
