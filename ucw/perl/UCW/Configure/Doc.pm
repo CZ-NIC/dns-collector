efaa9aa9201e5d35927b49be4167e02ca6ba9437
Michal Vaner <vorner@ucw.cz>
2008-10-31 15:57:21 +0100

    Doc system: Detect asciidoc
    And decide if we build documentation.

---
+++ ucw/perl/UCW/Configure/Doc.pm
@@ -0,0 +1,26 @@
+# UCW Library configuration system: documentation requirements
+# (c) 2008 Michal Vaner <vorner@ucw.cz>
+
+package UCW::Configure::Paths;
+use UCW::Configure;
+
+use strict;
+use warnings;
+
+Test("ASCII_DOC", "Checking for AsciiDoc", sub {
+	my $version = `asciidoc --version`;
+	return "none" if !defined $version || $version eq "";
+	my( $vnum ) = $version =~ / (\d+)\.\S*$/;
+	return "old" if $vnum < 7;
+	return "yes";
+});
+
+if(Get("ASCII_DOC") eq "yes") {
+	Set("CONFIG_DOC");
+} else {
+	Warn("Need asciidoc >= 7 to build documentation");
+	UnSet("CONFIG_DOC");
+}
+
+# We succeeded
+1;
