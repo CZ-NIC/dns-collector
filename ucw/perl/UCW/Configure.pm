e13a0599ac0a39ea63ca21a722c4d2db1d39af02
Martin Mares <mj@ucw.cz>
2008-11-03 23:48:38 +0100

    Configure: Moved interaction with pkg-config to a separate module (continued).
    Sorry, I have commited only a half of the change.

--- ucw/perl/UCW/Configure.pm
+++ ucw/perl/UCW/Configure.pm
@@ -16,7 +16,7 @@ BEGIN {
 	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 	$VERSION = 1.0;
 	@ISA = qw(Exporter);
-	@EXPORT = qw(&Init &Log &Notice &Warn &Fail &IsSet &IsGiven &Set &UnSet &Append &Override &Get &Test &Include &Finish &FindFile &TryFindFile &TryCmd &PkgConfig &TrivConfig &DebugDump &PostConfig &AtWrite);
+	@EXPORT = qw(&Init &Log &Notice &Warn &Fail &IsSet &IsGiven &Set &UnSet &Append &Override &Get &Test &Include &Finish &FindFile &TryFindFile &DebugDump &PostConfig &AtWrite);
 	@EXPORT_OK = qw();
 	%EXPORT_TAGS = ();
 }
@@ -206,91 +206,6 @@ sub Finish() {
 	for my $wr (@atwrites) {
 		&$wr();
 	}
-}
-
-sub TryCmd($) {
-	my ($cmd) = @_;
-	my $res = `$cmd`;
-	defined $res or return;
-	chomp $res;
-	return $res unless $?;
-	return;
-}
-
-sub maybe_manually($) {
-	my ($n) = @_;
-	if (IsGiven($n)) {
-		if (Get("$n")) { Log "YES (set manually)\n"; }
-		else { Log "NO (set manually)\n"; }
-		return 1;
-	}
-	return 0;
-}
-
-sub PkgConfig($@) {
-	my $pkg = shift @_;
-	my %opts = @_;
-	my $upper = $pkg; $upper =~ tr/a-z/A-Z/; $upper =~ s/[^0-9A-Z]+/_/g;
-	Log "Checking for package $pkg ... ";
-	maybe_manually("CONFIG_HAVE_$upper") and return Get("CONFIG_HAVE_$upper");
-	my $ver = TryCmd("pkg-config --modversion $pkg 2>/dev/null");
-	if (!defined $ver) {
-		Log("NONE\n");
-		return 0;
-	}
-	if (defined($opts{minversion})) {
-		my $min = $opts{minversion};
-		if (!defined TryCmd("pkg-config --atleast-version=$min $pkg")) {
-			Log("NO: version $ver is too old (need >= $min)\n");
-			return 0;
-		}
-	}
-	Log("YES: version $ver\n");
-	Set("CONFIG_HAVE_$upper" => 1);
-	Set("CONFIG_VER_$upper" => $ver);
-	my $cf = TryCmd("pkg-config --cflags $pkg");
-	Set("${upper}_CFLAGS" => $cf) if defined $cf;
-	my $lf = TryCmd("pkg-config --libs $pkg");
-	Set("${upper}_LIBS" => $lf) if defined $lf;
-	return 1;
-}
-
-sub ver_norm($) {
-	my ($v) = @_;
-	return join(".", map { sprintf("%05s", $_) } split(/\./, $v));
-}
-
-sub TrivConfig($@) {
-	my $pkg = shift @_;
-	my %opts = @_;
-	my $upper = $pkg; $upper =~ tr/a-z/A-Z/; $upper =~ s/[^0-9A-Z]+/_/g;
-	Log "Checking for package $pkg ... ";
-	maybe_manually("CONFIG_HAVE_$upper") and return Get("CONFIG_HAVE_$upper");
-	my $pc = $opts{script};
-	my $ver = TryCmd("$pc --version 2>/dev/null");
-	if (!defined $ver) {
-		Log("NONE\n");
-		return 0;
-	}
-	if (defined($opts{minversion})) {
-		my $min = $opts{minversion};
-		if (ver_norm($ver) lt ver_norm($min)) {
-			Log("NO: version $ver is too old (need >= $min)\n");
-			return 0;
-		}
-	}
-	Log("YES: version $ver\n");
-	Set("CONFIG_HAVE_$upper" => 1);
-	Set("CONFIG_VER_$upper" => $ver);
-
-	my $want = $opts{want};
-	defined $want or $want = ["cflags", "libs"];
-	for my $w (@$want) {
-		my $uw = $w; $uw =~ tr/a-z-/A-Z_/;
-		my $cf = TryCmd("$pc --$w");
-		Set("${upper}_${uw}" => $cf) if defined $cf;
-	}
-	return 1;
 }
 
 1;  # OK
