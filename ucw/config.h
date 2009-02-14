2ac260ae8694188410a408cd8c379a2fe45e4ee5
Martin Mares <mj@ucw.cz>
2009-02-15 00:21:09 +0100

    More documentation of <ucw/lib.h> and <ucw/config.h>.
    Still not complete.

--- ucw/config.h
+++ ucw/config.h
@@ -1,7 +1,7 @@
 /*
  *	UCW Library -- Configuration-Dependent Definitions
  *
- *	(c) 1997--2007 Martin Mares <mj@ucw.cz>
+ *	(c) 1997--2009 Martin Mares <mj@ucw.cz>
  *	(c) 2006 Robert Spalek <robert@ucw.cz>
  *
  *	This software may be freely distributed and used according to the terms
@@ -26,22 +26,22 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef uint8_t byte;			/* exactly 8 bits, unsigned */
-typedef uint8_t u8;			/* exactly 8 bits, unsigned */
-typedef int8_t s8;			/* exactly 8 bits, signed */
-typedef uint16_t u16;			/* exactly 16 bits, unsigned */
-typedef int16_t s16;			/* exactly 16 bits, signed */
-typedef uint32_t u32;			/* exactly 32 bits, unsigned */
-typedef int32_t s32;			/* exactly 32 bits, signed */
-typedef uint64_t u64;			/* exactly 64 bits, unsigned */
-typedef int64_t s64;			/* exactly 64 bits, signed */
+typedef uint8_t byte;			/** Exactly 8 bits, unsigned **/
+typedef uint8_t u8;			/** Exactly 8 bits, unsigned **/
+typedef int8_t s8;			/** Exactly 8 bits, signed **/
+typedef uint16_t u16;			/** Exactly 16 bits, unsigned **/
+typedef int16_t s16;			/** Exactly 16 bits, signed **/
+typedef uint32_t u32;			/** Exactly 32 bits, unsigned **/
+typedef int32_t s32;			/** Exactly 32 bits, signed **/
+typedef uint64_t u64;			/** Exactly 64 bits, unsigned **/
+typedef int64_t s64;			/** Exactly 64 bits, signed **/
 
-typedef unsigned int uns;		/* at least 32 bits */
-typedef u32 ucw_time_t;			/* seconds since UNIX epoch */
-typedef s64 timestamp_t;		/* milliseconds since UNIX epoch */
+typedef unsigned int uns;		/** A better pronounceable alias for `unsigned int` **/
+typedef u32 ucw_time_t;			/** Seconds since UNIX epoch **/
+typedef s64 timestamp_t;		/** Milliseconds since UNIX epoch **/
 
-#ifdef CONFIG_LARGE_FILES		/* File positions */
-typedef s64 ucw_off_t;
+#ifdef CONFIG_LARGE_FILES
+typedef s64 ucw_off_t;			/** File position (either 32- or 64-bit, depending on `CONFIG_LARGE_FILES`). **/
 #else
 typedef s32 ucw_off_t;
 #endif
--- ucw/lib.h
+++ ucw/lib.h
@@ -1,7 +1,7 @@
 /*
  *	The UCW Library -- Miscellaneous Functions
  *
- *	(c) 1997--2008 Martin Mares <mj@ucw.cz>
+ *	(c) 1997--2009 Martin Mares <mj@ucw.cz>
  *	(c) 2005 Tomas Valla <tom@ucw.cz>
  *	(c) 2006 Robert Spalek <robert@ucw.cz>
  *	(c) 2007 Pavel Charvat <pchar@ucw.cz>
@@ -16,61 +16,70 @@
 #include "ucw/config.h"
 #include <stdarg.h>
 
-/* Macros for handling structurues, offsets and alignment */
+/*** === Macros for handling structures, offsets and alignment ***/
 
-#define CHECK_PTR_TYPE(x, type) ((x)-(type)(x) + (type)(x))
-#define PTR_TO(s, i) &((s*)0)->i
-#define OFFSETOF(s, i) ((unsigned int) (uintptr_t) PTR_TO(s, i))
-#define SKIP_BACK(s, i, p) ((s *)((char *)p - OFFSETOF(s, i)))
+#define CHECK_PTR_TYPE(x, type) ((x)-(type)(x) + (type)(x))		/** Check that a pointer @x is of type @type. Fail compilation if not. **/
+#define PTR_TO(s, i) &((s*)0)->i					/** Return OFFSETOF() in form of a pointer. **/
+#define OFFSETOF(s, i) ((unsigned int) (uintptr_t) PTR_TO(s, i))	/** Offset of item @i from the start of structure @s **/
+#define SKIP_BACK(s, i, p) ((s *)((char *)p - OFFSETOF(s, i)))		/** Given a pointer @p to item @i of structure @s, return a pointer to the start of the struct. **/
+
+/**
+ * Align an integer @s to the nearest higher multiple of @a (which should be a power of two)
+ **/
 #define ALIGN_TO(s, a) (((s)+a-1)&~(a-1))
+
+/**
+ * Align a pointer @p to the nearest higher multiple of @s.
+ **/
 #define ALIGN_PTR(p, s) ((uintptr_t)(p) % (s) ? (typeof(p))((uintptr_t)(p) + (s) - (uintptr_t)(p) % (s)) : (p))
+
 #define UNALIGNED_PART(ptr, type) (((uintptr_t) (ptr)) % sizeof(type))
 
-/* Some other macros */
+/*** === Other utility macros ***/
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
-#define MAX(a,b) (((a)>(b))?(a):(b))
-#define CLAMP(x,min,max) ({ int _t=x; (_t < min) ? min : (_t > max) ? max : _t; })
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof(*(a)))
-#define STRINGIFY(x) #x
-#define STRINGIFY_EXPANDED(x) STRINGIFY(x)
-#define GLUE(x,y) x##y
-#define GLUE_(x,y) x##_##y
+#define MIN(a,b) (((a)<(b))?(a):(b))			/** Minimum of two numbers **/
+#define MAX(a,b) (((a)>(b))?(a):(b))			/** Maximum of two numbers **/
+#define CLAMP(x,min,max) ({ int _t=x; (_t < min) ? min : (_t > max) ? max : _t; })	/** Clip a number @x to interval [@min,@max] **/
+#define ABS(x) ((x) < 0 ? -(x) : (x))			/** Absolute value **/
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(*(a)))		/** The number of elements of an array **/
+#define STRINGIFY(x) #x					/** Convert macro parameter to a string **/
+#define STRINGIFY_EXPANDED(x) STRINGIFY(x)		/** Convert an expanded macro parameter to a string **/
+#define GLUE(x,y) x##y					/** Glue two tokens together **/
+#define GLUE_(x,y) x##_##y				/** Glue two tokens together, separating them by an underscore **/
 
-#define COMPARE(x,y) do { if ((x)<(y)) return -1; if ((x)>(y)) return 1; } while(0)
-#define REV_COMPARE(x,y) COMPARE(y,x)
+#define COMPARE(x,y) do { if ((x)<(y)) return -1; if ((x)>(y)) return 1; } while(0)		/** Numeric comparison function for qsort() **/
+#define REV_COMPARE(x,y) COMPARE(y,x)								/** Reverse numeric comparison **/
 #define COMPARE_LT(x,y) do { if ((x)<(y)) return 1; if ((x)>(y)) return 0; } while(0)
 #define COMPARE_GT(x,y) COMPARE_LT(y,x)
 
-#define	ROL(x, bits) (((x) << (bits)) | ((uns)(x) >> (sizeof(uns)*8 - (bits))))	/* Bitwise rotation of an uns to the left */
-#define	ROR(x, bits) (((uns)(x) >> (bits)) | ((x) << (sizeof(uns)*8 - (bits))))
+#define	ROL(x, bits) (((x) << (bits)) | ((uns)(x) >> (sizeof(uns)*8 - (bits))))		/** Bitwise rotation of an unsigned int to the left **/
+#define	ROR(x, bits) (((uns)(x) >> (bits)) | ((x) << (sizeof(uns)*8 - (bits))))		/** Bitwise rotation of an unsigned int to the right **/
 
-/* GCC Extensions */
+/*** === Shortcuts for GCC Extensions ***/
 
 #ifdef __GNUC__
 
 #undef inline
-#define NONRET __attribute__((noreturn))
-#define UNUSED __attribute__((unused))
-#define CONSTRUCTOR __attribute__((constructor))
-#define PACKED __attribute__((packed))
-#define CONST __attribute__((const))
-#define PURE __attribute__((pure))
-#define FORMAT_CHECK(x,y,z) __attribute__((format(x,y,z)))
-#define likely(x) __builtin_expect((x),1)
-#define unlikely(x) __builtin_expect((x),0)
+#define NONRET __attribute__((noreturn))				/** Function does not return **/
+#define UNUSED __attribute__((unused))					/** Variable/parameter is knowingly unused **/
+#define CONSTRUCTOR __attribute__((constructor))			/** Call function upon start of program **/
+#define PACKED __attribute__((packed))					/** Structure should be packed **/
+#define CONST __attribute__((const))					/** Function depends only on arguments **/
+#define PURE __attribute__((pure))					/** Function depends only on arguments and global vars **/
+#define FORMAT_CHECK(x,y,z) __attribute__((format(x,y,z)))		/** Checking of printf-like format strings **/
+#define likely(x) __builtin_expect((x),1)				/** Use `if (likely(@x))` if @x is almost always true **/
+#define unlikely(x) __builtin_expect((x),0)				/** Use `if (unlikely(@x))` to hint that @x is almost always false **/
 
 #if __GNUC__ >= 4 || __GNUC__ == 3 && __GNUC_MINOR__ >= 3
-#define ALWAYS_INLINE inline __attribute__((always_inline))
-#define NO_INLINE __attribute__((noinline))
+#define ALWAYS_INLINE inline __attribute__((always_inline))		/** Forcibly inline **/
+#define NO_INLINE __attribute__((noinline))				/** Forcibly uninline **/
 #else
 #define ALWAYS_INLINE inline
 #endif
 
 #if __GNUC__ >= 4
-#define LIKE_MALLOC __attribute__((malloc))
-#define SENTINEL_CHECK __attribute__((sentinel))
+#define LIKE_MALLOC __attribute__((malloc))				/** Function returns a "new" pointer **/
+#define SENTINEL_CHECK __attribute__((sentinel))			/** The last argument must be NULL **/
 #else
 #define LIKE_MALLOC
 #define SENTINEL_CHECK
@@ -136,38 +145,38 @@ void assert_failed_noinfo(void) NONRET;
 #define DBG(x,y...) do { } while(0)
 #endif
 
-/* Memory allocation */
-
-#define xmalloc ucw_xmalloc
-#define xrealloc ucw_xrealloc
-#define xfree ucw_xfree
+/*** === Memory allocation ***/
 
 /*
  * Unfortunately, several libraries we might want to link to define
  * their own xmalloc and we don't want to interfere with them, hence
  * the renaming.
  */
-void *xmalloc(uns) LIKE_MALLOC;
-void *xrealloc(void *, uns);
-void xfree(void *);
+#define xmalloc ucw_xmalloc
+#define xrealloc ucw_xrealloc
+#define xfree ucw_xfree
 
-void *xmalloc_zero(uns) LIKE_MALLOC;
-char *xstrdup(const char *) LIKE_MALLOC;
+void *xmalloc(uns) LIKE_MALLOC;			/** Allocate memory and die() if there is none. **/
+void *xrealloc(void *, uns);			/** Reallocate memory and die() if there is none. **/
+void xfree(void *);				/** Free memory allocated by xmalloc() or xrealloc(). **/
 
-/* timer.c */
+void *xmalloc_zero(uns) LIKE_MALLOC;		/** Allocate memory and fill it by zeroes. **/
+char *xstrdup(const char *) LIKE_MALLOC;	/** Make a xmalloc()'ed copy of a string. **/
 
-timestamp_t get_timestamp(void);
+/*** === Trivial timers (timer.c) ***/
 
-void init_timer(timestamp_t *timer);
-uns get_timer(timestamp_t *timer);
-uns switch_timer(timestamp_t *oldt, timestamp_t *newt);
+timestamp_t get_timestamp(void);		/** Get current time as a millisecond timestamp. **/
 
-/* random.c */
+void init_timer(timestamp_t *timer);		/** Initialize a timer. **/
+uns get_timer(timestamp_t *timer);		/** Get the number of milliseconds since last init/get of a timer. **/
+uns switch_timer(timestamp_t *oldt, timestamp_t *newt);	/** Stop ticking of one timer and resume another. **/
 
-uns random_u32(void);
-uns random_max(uns max);
-u64 random_u64(void);
-u64 random_max_u64(u64 max);
+/*** === Random numbers (random.c) ***/
+
+uns random_u32(void);				/** Return a pseudorandom 32-bit number. **/
+uns random_max(uns max);			/** Return a pseudorandom 32-bit number in range [0,@max). **/
+u64 random_u64(void);				/** Return a pseudorandom 64-bit number. **/
+u64 random_max_u64(u64 max);			/** Return a pseudorandom 64-bit number in range [0,@max). **/
 
 /* mmap.c */
 
