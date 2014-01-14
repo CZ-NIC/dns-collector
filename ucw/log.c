82d6c472e0e742e3c5d2572855710b0a4a2e619b
Pavel Charvat <pchar@ucw.cz>
2014-01-14 08:58:15 +0100

    LibUCW: Implemented UCW_DIE_BY_ABORT environment switch.

--- ucw/log.c
+++ ucw/log.c
@@ -337,6 +337,9 @@ do_die(void)
 #ifdef DEBUG_DIE_BY_ABORT
   abort();
 #else
+  const char *env = getenv("CONFIG_UCW_ENV_VAR_DIE_BY_ABORT");
+  if (env && *env)
+    abort();
   exit(1);
 #endif
 }
