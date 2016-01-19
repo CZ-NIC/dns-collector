8ad3370777fcfef2bbac196d8112f657ca17b6e3
Tomas Gavenciak <gavento@ucw.cz>
2016-01-19 16:59:00 +0100

    Update dumpfile reopening, check link layer

--- collector/collector.c
+++ collector/collector.c
@@ -23,10 +23,7 @@ dns_collector_create(const dns_collector_config_t *conf)
 void
 dns_collector_destroy(dns_collector_t *col)
 { 
-    if (col->pcap_dumper) {
-        pcap_dump_close(col->pcap_dumper);
-        col->pcap_dumper = NULL;
-    }
+    dns_collector_dump_close(col);
 
     if (col->pcap) {
         pcap_close(col->pcap);
@@ -45,12 +42,6 @@ dns_collector_open_pcap(dns_collector_t *col, const char *pcap_fname)
     char pcap_errbuf[PCAP_ERRBUF_SIZE];
 
     assert(col);
-    if (col->pcap_dumper) {
-        // non-null pcap_dumper_fname later indicates to reopen the same file
-        assert(col->pcap_dumper_fname[0] != 0);
-        pcap_dump_close(col->pcap_dumper);
-        col->pcap_dumper = NULL;
-    } 
 
     if (col->pcap) {
         pcap_close(col->pcap);
@@ -58,43 +49,59 @@ dns_collector_open_pcap(dns_collector_t *col, const char *pcap_fname)
     }
 
     col->pcap = pcap_open_offline(pcap_fname, pcap_errbuf);
+
     if (!col->pcap) {
         fprintf(stderr, "libpcap error: %s\n", pcap_errbuf);
         return -1;
     }
 
-    if (col->pcap_dumper_fname[0]) {
-        // reopen existing dump file
-//        col->pcap_dumper = pcap_dump_open_append(col->pcap, &(col->pcap_dumper_fname)); TODO: old pcap version :(
-        if (!col->pcap_dumper) {
-            fprintf(stderr, "libpcap error: %s\n", pcap_geterr(col->pcap));
-            return -1;
-        }
-    } else {
-        // open new dump file if configured
-        if (col->config->dumpfile_base) {
-            // TODO: changing names, match frame name
-            snprintf(col->pcap_dumper_fname, DNSCOL_MAX_FNAME_LEN, "%s.pcap", col->config->dumpfile_base);
-            col->pcap_dumper = pcap_dump_open(col->pcap, col->pcap_dumper_fname);
-            if (!col->pcap_dumper) {
-                fprintf(stderr, "libpcap error: %s\n", pcap_geterr(col->pcap));
-                return -1;
-            }
-        }
+    if (pcap_datalink(col->pcap) != DLT_RAW) {
+        fprintf(stderr, "error: pcap with link %s not supported\n", pcap_datalink_val_to_name(pcap_datalink(col->pcap)));
+        pcap_close(col->pcap);
+        col->pcap = NULL;
+        return -1;
     }
 
     return 0;
+}
+
+
+int
+dns_collector_dump_open(dns_collector_t *col, const char *dump_fname)
+{
+    assert(col);
+
+    dns_collector_dump_close(col);
+    col->pcap_dump = pcap_dump_open(col->pcap, dump_fname);
+
+    if (!col->pcap_dump) {
+        fprintf(stderr, "libpcap error: %s\n", pcap_geterr(col->pcap));
+        return -1;
+    }
+
+    return 0;
+}
+
+void
+dns_collector_dump_close(dns_collector_t *col)
+{
+    assert(col);
+    
+    if (col->pcap_dump) {
+        pcap_dump_close(col->pcap_dump);
+        col->pcap_dump = NULL;
+    }
 }
 
 int
 dns_collector_dump_packet(dns_collector_t *col, struct pcap_pkthdr *pkt_header, const u_char *pkt_data)
 {
     assert(col);
-    if (!col->pcap_dumper) {
+    if (!col->pcap_dump) {
         return -1;
     }
 
-    pcap_dump((u_char *)(col->pcap_dumper), pkt_header, pkt_data);
+    pcap_dump((u_char *)(col->pcap_dump), pkt_header, pkt_data);
     col->stats.packets_dumped++;
     // TODO: frame stats
 
@@ -114,16 +121,28 @@ dns_collector_next_packet(dns_collector_t *col)
     }
 
     r = pcap_next_ex(col->pcap, &pkt_header, &pkt_data);
-    if (r <= 0)
+    if (r <= 0) {
         return r;
+    }
     assert(r == 1); 
+
     col->stats.packets_captured++;
     // TODO: frame stats
 
+    r = dns_collector_process_packet(col, pkt_header, pkt_data);
 
+    if (r == 0)
+        return 1;
+    return -1;
+}
+
+int
+dns_collector_process_packet(dns_collector_t *col, struct pcap_pkthdr *pkt_header, const u_char *pkt_data)
+{
     // TODO: add some processing :)
-    r = dns_collector_dump_packet(col, pkt_header, pkt_data);
-    return 1;
+    dns_collector_dump_packet(col, pkt_header, pkt_data);
+    
+    return 0;
 }
 
 void
