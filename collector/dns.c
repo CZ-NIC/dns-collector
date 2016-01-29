8c867896bf1629648198c82de30cfe031b7bee28
Tomas Gavenciak <gavento@ucw.cz>
2016-01-29 12:52:54 +0100

    Implement printable qname conversion.

--- collector/dns.c
+++ collector/dns.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <netinet/in.h>
 
 #include "common.h"
 #include "dns.h"
@@ -24,5 +26,54 @@ dns_query_check(u_char *query, uint32_t caplen)
         }
     }
     return -1;
+}
+
+int
+dns_query_to_printable(u_char *query, char *output)
+{
+    assert(query && output);
+
+    int rem = 0;
+    int bad = 0;
+
+    // first label: do not start output with a dot
+    if (*query == '\0') {
+        *output = '\0';
+        return bad;
+    } else {
+        assert((*query & 0xc0) == 0);
+        rem = *query;
+        query++;
+    }
+
+    for(;; query++, output++) {
+        if (rem == 0) {
+            if (*query == '\0')
+            {
+                *output = '\0';
+                return bad;
+            }
+            assert((*query & 0xc0) == 0);
+            *output = '.';
+            rem = *query;
+        } else {
+            if (((*query >= 'a') && (*query <= 'z')) ||
+                ((*query >= 'A') && (*query <= 'Z')) ||
+                ((*query >= '0') && (*query <= '9')) ||
+                (*query == '-')) {
+                *output = *query;
+            } else {
+                bad++;
+                *output = '#';
+            }
+            rem --;
+        }
+    }
+}
+
+uint16_t
+dns_hdr_flags(const struct dns_hdr *hdr)
+{ 
+    return ntohs(((const uint16_t*)hdr)[1]);
 }
 
