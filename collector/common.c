bb099645c081e864a6f587b259423cfcfcd316da
Tomas Gavenciak <gavento@ucw.cz>
2016-02-18 23:02:20 +0100

    Add time formatting

--- collector/common.c
+++ collector/common.c
@@ -1,8 +1,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "common.h"
+
 
 void dns_die_f(int line, const char *file, const char *func, const char *msg)
 {
@@ -10,11 +12,13 @@ void dns_die_f(int line, const char *file, const char *func, const char *msg)
     exit(1);
 }
 
+
 dns_us_time_t
 dns_us_time_from_timeval(const struct timeval *t)
 {
     return t->tv_usec + (1000000 * t->tv_sec);
 }
+
 
 dns_us_time_t
 dns_us_time_from_timespec(const struct timespec *t)
@@ -22,15 +26,29 @@ dns_us_time_from_timespec(const struct timespec *t)
     return (t->tv_nsec / 1000) + (1000000 * t->tv_sec);
 }
 
+
 double
 dns_us_time_to_fsec(dns_us_time_t t)
 {
     return ((double)t) / 1000000.0;
 }
 
+
 dns_us_time_t
 dns_fsec_to_us_time(double s)
 {
     return (dns_us_time_t)(s * 1000000.0);
 }
+
+
+size_t
+dns_us_time_strftime(char *s, size_t max, const char *format, dns_us_time_t time)
+{
+    struct tm tmp_tm;
+    time_t tsec = time / 1000000;
+    struct tm *res = gmtime_r(&tsec, &tmp_tm);
+    assert(res);
+    return strftime(s, max, format, &tmp_tm);
+}
+
 
