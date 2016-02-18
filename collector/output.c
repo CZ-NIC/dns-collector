923e298befb977d464a23ec407a3daa8f7d59fcf
Tomas Gavenciak <gavento@ucw.cz>
2016-02-18 23:35:18 +0100

    Mostly done output system and CSV

--- collector/output.c
+++ collector/output.c
@@ -1,5 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/limits.h>
+#include <errno.h>
+
+
 #include "common.h"
 #include "output.h"
+
+
+const char *dns_output_field_names[] = {
+    "flags",
+    "client_addr",
+    "client_port",
+    "server_addr",
+    "server_port",
+    "id",
+    "qname",
+    "qtype",
+    "qclass",
+    "request_time_us",
+    "request_flags",
+    "request_length",
+    "response_time_us",
+    "response_flags",
+    "response_length",
+    NULL,
+};
 
 
 char *dns_output_init(struct dns_output *s)
@@ -12,13 +40,76 @@ char *dns_output_init(struct dns_output *s)
 
 char *dns_output_commit(struct dns_output *s)
 {
-    if (s->period_sec < 0.001)
-        return "'period' too small, minimum 0.001 sec.";
-    s->period = dns_fsec_to_us_time(s->period_sec);
+    if (s->period_sec < 0.000001)
+        s->period = 0;
+    else
+        s->period = dns_fsec_to_us_time(s->period_sec);
 
     if (!s->path_template)
         return "'path_template' needed in output.";
 
     return NULL;
 }
+
+
+void
+dns_output_open(struct dns_output *out, dns_us_time_t time)
+{
+    assert(out && !out->f && out->manage_files);
+
+    if (time == DNS_NO_TIME) {
+        msg(L_ERROR, "[BUG] Unable to open file with time==DNS_NO_TIME.");
+        return;
+    }
+
+    char path[PATH_MAX];
+    size_t l = dns_us_time_strftime(path, PATH_MAX, out->path_template, time);
+
+    if (l == 0)
+        die("Expanded filename '%s' too long.", out->path_template);
+        
+    out->f = fopen(path, "w");
+    if (!out->f)
+        die("Unable to open output file '%s': %s.", path, strerror(errno));
+    out->f_time_opened = time;
+
+    if (out->start_file)
+          out->start_file(out, time);
+}
+
+
+void
+dns_output_close(struct dns_output *out, dns_us_time_t time)
+{
+    assert(out && out->f && out->manage_files && time != DNS_NO_TIME);
+
+    if (out->finish_file)
+          out->finish_file(out, time);
+
+    fclose(out->f);
+    out->f = NULL;
+}
+
+
+void
+dns_output_write(struct dns_output *out, const char *buf, size_t len, dns_us_time_t time)
+{
+    assert(out && buf && out->manage_files);
+
+    // check if we need to switch output files
+    if ((time != DNS_NO_TIME) && (out->f) && (time >= out->f_time_opened + out->period))
+        dns_output_close(out, time);
+
+    // open output if none open
+    if (!out->f) {
+        dns_output_open(out, time);
+        if (!out->f)
+            return;
+    }
+
+    if (fwrite(buf, len, 1, out->f) != len) {
+        msg(L_ERROR, "IO error %d writing to an output file.", ferror(out->f));
+    }
+}
+
 
--- collector/output_csv.c
+++ collector/output_csv.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <ucw/lib.h>
 #include <ucw/conf.h>
 
@@ -11,35 +12,91 @@ struct dns_output_csv {
 
     char *separator;
     int header;
-
-    FILE *f;
-    dns_us_time_t f_opened;
+    uint32_t fields;
 };
 
+#define DNS_OUTPUT_CVS_LINEMAX 512
 
+/**
+ * Callback for cvs_output, writes CVS header.
+ */
+void
+dns_output_csv_start_file(struct dns_output *out0, dns_us_time_t time UNUSED)
+{
+    struct dns_output_csv *out = (struct dns_output_csv *) out0;
+    char buf[DNS_OUTPUT_CVS_LINEMAX];
+    char *p = buf;
+    int first = 1;
+
+    for (int f = 0; f < dns_of_LAST; f++) {
+        if (out->fields & (1 << f)) {
+            if (!first)
+                *(p++) = '|';
+            else
+                first = 0;
+
+            size_t l = strlen(dns_output_field_names[f]);
+            memcpy(p, dns_output_field_names[f], l);
+            p += l;
+        }
+    }
+
+    *(p++) = '\n';
+    dns_output_write(out0, buf, (p - buf), DNS_NO_TIME);
+}
+
+
+/**
+ * Callback for cvs_output, writes singe CVS line.
+ */
 static dns_ret_t
 dns_output_csv_write_packet(struct dns_output *out0, dns_packet_t *pkt)
 {
-    // TODO: open, reopen, write
-}
-
-
-static void
-dns_output_csv_close(struct dns_output *out0)
-{
     struct dns_output_csv *out = (struct dns_output_csv *) out0;
-    if (out->f)
-        fclose(out->f);
+    char buf[DNS_OUTPUT_CVS_LINEMAX];
+    char *p = buf;
+    int first = 1;
+
+    for (int f = 0; f < dns_of_LAST; f++) {
+        if (out->fields & (1 << f)) {
+            if (!first)
+                *(p++) = '|';
+            else
+                first = 0;
+
+            switch (f) {
+                case dns_of_flags:
+                    p += sprintf(p, "%d", dns_packet_get_output_flags(pkt));
+                    break;
+                case dns_of_qname:
+                    p += sprintf(p, "%s", pkt->dns_qname_string);
+                    break;
+                // TODO: implement the rest
+                default:
+                    p += '?';
+            }
+        }
+    }
+
+    *(p++) = '\n';
+    dns_output_write(out0, buf, (p - buf), pkt->ts);
+
+    return DNS_RET_OK;
 }
 
 
+/**
+ * Helper for configuration init.
+ */
 static char *
 dns_output_csv_conf_init(void *data)
 {
     struct dns_output_csv *s = (struct dns_output_csv *) data;
 
     s->base.write_packet = dns_output_csv_write_packet;
-    s->base.close = dns_output_csv_close;
+    s->base.start_file = dns_output_csv_start_file;
+    s->base.manage_files = 1;
+
     s->header = 1;
     s->separator = "|";
 
@@ -47,6 +104,9 @@ dns_output_csv_conf_init(void *data)
 }
 
 
+/**
+ * Helper for configuration post-processing and validation.
+ */
 static char *
 dns_output_csv_conf_commit(void *data)
 {
@@ -68,6 +128,7 @@ struct cf_section dns_output_csv_section = {
         CF_DOUBLE("period", PTR_TO(struct dns_output, period_sec)),
         CF_STRING("separator", PTR_TO(struct dns_output_csv, separator)),
         CF_INT("header", PTR_TO(struct dns_output_csv, header)),
+        CF_BITMAP_LOOKUP("fields", PTR_TO(struct dns_output_csv, fields), dns_output_field_names),
         CF_END
     }
 };
