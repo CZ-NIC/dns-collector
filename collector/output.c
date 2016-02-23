69bb3e12627d4a69f9523afc47bba9af9d1c085d
Tomas Gavenciak <gavento@ucw.cz>
2016-02-23 14:57:23 +0100

    Big output update: compression, pcap
    * transparent LZ4 compression, configurable
    * own PCAP dump (compression + libpcap independent)

--- collector/output.c
+++ collector/output.c
@@ -5,13 +5,46 @@
 #include <linux/limits.h>
 #include <errno.h>
 
+#include <lz4.h>
+#include <lz4frame.h>
+#define LZ4_HEADER_SIZE 20
+#define LZ4_FOOTER_SIZE 8
+
 #include "common.h"
 #include "output.h"
+
+
+const char *dns_output_compression_names[] = {
+      "none", "lz4-fast", "lz4-med", "lz4-best", NULL
+};
+
+
+static LZ4F_preferences_t lz4fast_preferences = {
+    { LZ4F_max64KB, LZ4F_blockLinked, LZ4F_noContentChecksum, LZ4F_frame, 0, { 0, 0 } },
+    0, 0,
+    { 0, 0, 0, 0 },  /* reserved */
+};
+
+
+static LZ4F_preferences_t lz4med_preferences = {
+    { LZ4F_max256KB, LZ4F_blockLinked, LZ4F_noContentChecksum, LZ4F_frame, 0, { 0, 0 } },
+    4, 0,
+    { 0, 0, 0, 0 },  /* reserved */
+};
+
+
+static LZ4F_preferences_t lz4best_preferences = {
+    { LZ4F_max1MB, LZ4F_blockLinked, LZ4F_noContentChecksum, LZ4F_frame, 0, { 0, 0 } },
+    9, 0,
+    { 0, 0, 0, 0 },  /* reserved */
+};
 
 
 char *dns_output_init(struct dns_output *out)
 {
     out->period_sec = 300.0; // 5 min
+
+    out->compression = dns_oc_none;
 
     return NULL;
 }
@@ -27,9 +60,25 @@ char *dns_output_commit(struct dns_output *out)
     if (!out->path_template)
         return "'path_template' needed in output.";
 
+    if (out->compression >= dns_oc_LAST)
+        return "Invalid compression.";
+
     return NULL;
 }
 
+#define DNS_OUTPUT_LZ4_WRITE_SIZE (1024 * 4)
+
+static void
+dns_output_writebuf_helper(const char *buf, size_t len, FILE *f)
+{
+    size_t n, pos = 0;
+    while (pos < len) {
+        n = fwrite(buf + pos, 1, len - pos, f);
+        if ((n < len - pos) && ferror(f))
+            msg(L_ERROR, "Output write error %d", ferror(f));
+        pos += n;
+    }
+}
 
 void
 dns_output_open(struct dns_output *out, dns_us_time_t time)
@@ -47,6 +96,29 @@ dns_output_open(struct dns_output *out, dns_us_time_t time)
         die("Unable to open output file '%s': %s.", path, strerror(errno));
     out->f_time_opened = time;
 
+    if (out->compression == dns_oc_lz4fast || out->compression == dns_oc_lz4med || out->compression == dns_oc_lz4best) {
+        size_t n;
+
+        n = LZ4F_createCompressionContext(&(out->lz4_ctx), LZ4F_VERSION);
+        if (LZ4F_isError(n))
+            die("LZ4 error: %s", LZ4F_getErrorName(n));
+
+        switch (out->compression) {
+            case dns_oc_lz4fast: out->lz4_prefs = &lz4fast_preferences; break;
+            case dns_oc_lz4med: out->lz4_prefs = &lz4med_preferences; break;
+            case dns_oc_lz4best: out->lz4_prefs = &lz4best_preferences; break;
+            default: assert(0);
+        }
+
+        out->lz4_buf_len = LZ4F_compressBound(DNS_OUTPUT_LZ4_WRITE_SIZE, out->lz4_prefs) + LZ4_HEADER_SIZE + LZ4_FOOTER_SIZE;
+        out->lz4_buf = (char *)xmalloc(out->lz4_buf_len);
+
+        n = LZ4F_compressBegin(out->lz4_ctx, out->lz4_buf, out->lz4_buf_len, out->lz4_prefs);
+        if (LZ4F_isError(n))
+            die("LZ4 error: %s", LZ4F_getErrorName(n));
+        dns_output_writebuf_helper(out->lz4_buf, n, out->f);
+    }
+
     if (out->start_file)
           out->start_file(out, time);
 }
@@ -59,6 +131,19 @@ dns_output_close(struct dns_output *out, dns_us_time_t time)
 
     if (out->finish_file)
           out->finish_file(out, time);
+
+    if (out->compression == dns_oc_lz4fast || out->compression == dns_oc_lz4med || out->compression == dns_oc_lz4best) {
+        size_t n;
+
+        n = LZ4F_compressEnd(out->lz4_ctx, out->lz4_buf, out->lz4_buf_len, NULL);
+        if (LZ4F_isError(n))
+            die("LZ4 error: %s", LZ4F_getErrorName(n));
+        dns_output_writebuf_helper(out->lz4_buf, n, out->f);
+
+        LZ4F_freeCompressionContext(out->lz4_ctx);
+        xfree(out->lz4_buf);
+        out->lz4_buf_len = 0;
+    }
 
     fclose(out->f);
     out->f = NULL;
@@ -85,13 +170,22 @@ dns_output_write(struct dns_output *out, const char *buf, size_t len)
 {
     assert(out && buf);
 
-    if ((len > 1) && (out->f)) {
-        size_t l = fwrite(buf, len, 1, out->f);
-        if (l != 1) {
-            msg(L_ERROR, "IO error %d writing to an output file.", ferror(out->f));
-            // TODO: Do something else?
-        }
+    if (out->compression == dns_oc_none) {
+        dns_output_writebuf_helper(buf, len, out->f);
+        return;
     }
+
+    if (out->compression == dns_oc_lz4fast || out->compression == dns_oc_lz4med || out->compression == dns_oc_lz4best) {
+        while(len > 0) {
+            size_t n, wr = MIN(len, DNS_OUTPUT_LZ4_WRITE_SIZE);
+            n = LZ4F_compressUpdate(out->lz4_ctx, out->lz4_buf, out->lz4_buf_len, buf, wr, NULL);
+            dns_output_writebuf_helper(out->lz4_buf, n, out->f);
+            buf += wr;
+            len -= wr;
+        }
+        return;
+    }
+    die("unreachable");
 }
 
 
--- collector/output_pcap.c
+++ collector/output_pcap.c
@@ -13,23 +13,62 @@ struct dns_output_pcap {
     struct dns_output base;
 
     uint32_t dump_reasons;
-    pcap_dumper_t *dumper;
+};
+
+/**
+ * Local definition of PCAP packet header to decouple from libpcap and allow compression.
+ * From libtrace/lib/libtrace_int.h
+ */
+
+struct dns_pcapfile_header {
+    uint32_t magic_number;   /* magic number */
+    uint16_t version_major;  /* major version number */
+    uint16_t version_minor;  /* minor version number */
+    int32_t  thiszone;       /* GMT to local correction */
+    uint32_t sigfigs;        /* timestamp accuracy */
+    uint32_t snaplen;        /* aka "wirelen" */
+    uint32_t linktype;        /* data link type */
+};
+
+/**
+ * Local definition of PCAP file header to decouple from libpcap and allow compression.
+ * From libtrace/lib/format_pcapfile.c
+ */
+
+struct dns_pcapfile_pkt_header {
+    uint32_t ts_sec;      /* Seconds portion of the timestamp */
+    uint32_t ts_usec;     /* Microseconds portion of the timestamp */
+    uint32_t caplen;      /* Capture length of the packet */
+    uint32_t wirelen;     /* The wire length of the packet */
 };
 
 /**
  * Callback for pcap_output, writes PCAP header.
+ * From libtrace/lib/format_pcapfile.c
  */
 void
 dns_output_pcap_start_file(struct dns_output *out0, dns_us_time_t time UNUSED)
 {
     assert(out0 && out0->col);
 
-    struct dns_output_pcap *out = (struct dns_output_pcap *) out0;
-    out->dumper = pcap_dump_fopen(out0->col->pcap, out0->f);
+    struct dns_output_pcap *out UNUSED = (struct dns_output_pcap *) out0;
+
+    struct dns_pcapfile_header hdr = {
+        .magic_number = 0xa1b2c3d4,
+        .version_major = 2,
+        .version_minor = 4,
+        .thiszone = 0,
+        .snaplen = out0->col->config->capture_limit,
+        .sigfigs = 0,
+        .linktype = 101, // LINKTYPE_RAW
+    };
+    dns_output_write(out0, (void *)&hdr, sizeof(hdr));
 }
 
 /**
  * Callback for pcap_output, writes singe dropped packet.
+ *
+ * NOTE: Writing packet header stolen from pcap_dump() in libpcap/sf-pcap.c to allow write-time compression.
  */
 static dns_ret_t
 dns_output_pcap_drop_packet(struct dns_output *out0, dns_packet_t *pkt, enum dns_drop_reason reason)
@@ -39,14 +78,17 @@ dns_output_pcap_drop_packet(struct dns_output *out0, dns_packet_t *pkt, enum dns
     dns_output_check_rotation(out0, pkt->ts);
 
     // TODO: check dump (soft/hard) quota?
+
     if (out->dump_reasons & (1 << reason))
     {
-        const struct pcap_pkthdr hdr = {
-            .ts = {.tv_sec = pkt->ts / 1000000, .tv_usec = pkt->ts % 1000000},
+        struct dns_pcapfile_pkt_header sf_hdr = {
+            .ts_sec = pkt->ts / 1000000,
+            .ts_usec = pkt->ts % 1000000,
             .caplen = pkt->pkt_caplen,
-            .len = pkt->pkt_len,
+            .wirelen = pkt->pkt_len,
         };
-        pcap_dump((u_char *)(out->dumper), &hdr, pkt->pkt_data);
+        dns_output_write(out0, (void *)&sf_hdr, sizeof(sf_hdr));
+        dns_output_write(out0, (void *)pkt->pkt_data, sf_hdr.caplen);
     }
 
     return DNS_RET_OK;
@@ -86,8 +128,7 @@ struct cf_section dns_output_pcap_section = {
     .init = &dns_output_pcap_conf_init,
     .commit = &dns_output_pcap_conf_commit,
     CF_ITEMS {
-        CF_STRING("path_template", PTR_TO(struct dns_output, path_template)),
-        CF_DOUBLE("period", PTR_TO(struct dns_output, period_sec)),
+        CF_DNS_OUTPUT_COMMON,
         CF_BITMAP_LOOKUP("dump_reasons", PTR_TO(struct dns_output_pcap, dump_reasons), dns_drop_reason_names),
         CF_END
     }
