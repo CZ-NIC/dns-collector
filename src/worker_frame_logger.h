f68d033377d867238f603c4b1d50f18758ef0636
Tomas Gavenciak <gavento@ucw.cz>
2016-07-29 17:26:25 +0200

    Rename packet_frame_logger to worker_frame_logger

--- src/worker_frame_logger.h
+++ src/worker_frame_logger.h
@@ -9,7 +9,7 @@ struct dns_frame_queue;
  * A "logger" worker structure passing packets from one queue to another.
  * Mostly for debugging.
  */
-struct dns_packet_frame_logger {
+struct dns_worker_frame_logger {
     /** Input and output queue. Output may be NULL (discard). */
     struct dns_frame_queue *in, *out;
     /** Name for logging, owned */
@@ -23,25 +23,25 @@ struct dns_packet_frame_logger {
 /**
  * Create a logger. The output queue is optional.
  */
-struct dns_packet_frame_logger *
-dns_packet_frame_logger_create(const char *name, struct dns_frame_queue *in, struct dns_frame_queue *out);
+struct dns_worker_frame_logger *
+dns_worker_frame_logger_create(const char *name, struct dns_frame_queue *in, struct dns_frame_queue *out);
 
 /**
  * Wait for a logger thread to stop.
  */
 void
-dns_packet_frame_logger_finish(struct dns_packet_frame_logger *l);
+dns_worker_frame_logger_finish(struct dns_worker_frame_logger *l);
 
 /**
  * Destroy a logger. The logger must not be running!
  */
 void
-dns_packet_frame_logger_destroy(struct dns_packet_frame_logger *l);
+dns_worker_frame_logger_destroy(struct dns_worker_frame_logger *l);
 
 /**
  * Start the logger thread. The logger must not be already running!
  */
 void
-dns_packet_frame_logger_start(struct dns_packet_frame_logger *l);
+dns_worker_frame_logger_start(struct dns_worker_frame_logger *l);
 
 #endif /* DNSCOL_PACKET_FRAME_LOGGER_H */
